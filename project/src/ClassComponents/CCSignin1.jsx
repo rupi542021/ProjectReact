dddc51533f4f7ae24dde26a3673e839b6419a8a1
yuviBA <77380780+yuviBA@users.noreply.github.com>
2021-02-21 18:23:43 +0200

    handle exceptions signin1

--- project/src/ClassComponents/CCSignin1.jsx
+++ project/src/ClassComponents/CCSignin1.jsx
@@ -9,132 +9,150 @@ import background from "../img/background.jpg";
 
 class CCSignin1 extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
-        fields: {},
-        errors: {}
+      fields: {},
+      errors: {}
     }
- }
+  }
 
- handleValidation(){
-     let fields = this.state.fields;
-     let errors = {};
-     let formIsValid = true;
+  handleValidation() {
+    let fields = this.state.fields;
+    let errors = {};
+    let formIsValid = true;
 
-     //Email
-     if(!fields["email"]){
+    //Email
+    if (!fields["email"]) {
+      formIsValid = false;
+      errors["email"] = "Cannot be empty";
+    }
+
+    if (typeof fields["email"] !== "undefined") {
+      let lastAtPos = fields["email"].lastIndexOf('@');
+      let lastDotPos = fields["email"].lastIndexOf('.');
+
+      if (!(lastAtPos < lastDotPos && lastAtPos > 0 && fields["email"].indexOf('@@') == -1 && lastDotPos > 2 && (fields["email"].length - lastDotPos) > 2)) {
         formIsValid = false;
-        errors["email"] = "Cannot be empty";
-     }
+        errors["email"] = "Email is not valid";
+      }
+    }
 
-     if(typeof fields["email"] !== "undefined"){
-        let lastAtPos = fields["email"].lastIndexOf('@');
-        let lastDotPos = fields["email"].lastIndexOf('.');
-
-        if (!(lastAtPos < lastDotPos && lastAtPos > 0 && fields["email"].indexOf('@@') == -1 && lastDotPos > 2 && (fields["email"].length - lastDotPos) > 2)) {
-           formIsValid = false;
-           errors["email"] = "Email is not valid";
-         }
-    }  
-
-    this.setState({errors: errors});
+    this.setState({ errors: errors });
     return formIsValid;
-}
- 
-contactSubmit(e){
-     e.preventDefault();
+  }
 
-     if(this.handleValidation()){
-         console.log(this.state.fields["email"].toLowerCase())
-         
-    this.apiUrl='https://localhost:44325/API/students?email='+this.state.fields["email"].toLowerCase();
-    console.log('GETstart');
-    fetch(this.apiUrl,
-      {
-        method: 'GET',
-        headers: new Headers({
-          'Content-Type': 'application/json; charset=UTF-8',
-          'Accept': 'application/json; charset=UTF-8'
+  contactSubmit(e) {
+    e.preventDefault();
+
+    if (this.handleValidation()) {
+      console.log(this.state.fields["email"].toLowerCase())
+
+      this.apiUrl = 'https://localhost:44325/API/students?email=' + this.state.fields["email"].toLowerCase();
+      console.log('GETstart');
+      fetch(this.apiUrl,
+        {
+          method: 'GET',
+          headers: new Headers({
+            'Content-Type': 'application/json; charset=UTF-8',
+            'Accept': 'application/json; charset=UTF-8'
+          })
         })
-      })
-      .then((res)=>{
-         console.log('res=', res);
-         //console.log('res.status', res.status);
-         console.log('res.ok', res.ok);
-        return res.json();
-      })
-      .then(
-        (result) => {
+        .then((res) => {
+          console.log('res=', res);
+          console.log('res.ok', res.ok);
+          if (!res.ok) {
+            if (res.status === 404) {
+              throw Error('כתובת המייל אינה נמצאת במערכת')
+            }
+            if (res.status === 400) {
+              throw Error('כתובת המייל כבר נמצאת במערכת')
+            }
+
+            if (res.status === 500) {
+              throw Error('בעיה בהתחברות לשרת')
+            }
+          }
+          return res.json();
+        })
+        .then(
+          (result) => {
             console.log("fetch btnFetchGetStudents= ", result);
-            if(result.Mail!==null)
-            {
+            //if (result.Mail !== null) {
               localStorage.setItem('student', JSON.stringify(result));
               Swal.fire({
-                    title: 'אימות המייל בוצע בהצלחה!',
-                    icon: 'success',
-                    iconHtml: '',
-                    confirmButtonText: 'המשך',
-                    showCloseButton: true
-                  }).then(() => {
-            
-                    this.props.history.push("/signin2");
-            
-                  });
+                title: 'אימות המייל בוצע בהצלחה!',
+                icon: 'success',
+                iconHtml: '',
+                confirmButtonText: 'המשך',
+                showCloseButton: true
+              }).then(() => {
+
+                this.props.history.push("/signin2");
+
+              });
             }
-            else {
-                Swal.fire({
-                  title: 'כתובת המייל אינה נמצאת במערכת',
-                  icon: 'error',
-                  iconHtml: '',
-                  confirmButtonText: 'סגור',
-                  showCloseButton: true
-                })
-              }
-          },
-        (error) => {
-          console.log("err post=", error);
-        });
-    console.log('end');
+          //  else {
+            //   Swal.fire({
+            //     title: 'כתובת המייל אינה נמצאת במערכת',
+            //     icon: 'error',
+            //     iconHtml: '',
+            //     confirmButtonText: 'סגור',
+            //     showCloseButton: true
+            //   })
+            // }
+         // }
+        )
+        .catch((error) => {
+          console.log("err get=", error.message);
+          Swal.fire({
+                title: error.message,
+                icon: 'error',
+                iconHtml: '',
+                confirmButtonText: 'סגור',
+                showCloseButton: true
+              })
+        })
+      console.log('end');
 
-        //alert("Form submitted");
-     }
-     else{
-        //alert("Form has errors.")
-     }
+      //alert("Form submitted");
+    }
+    //  else{
+    //     //alert("Form has errors.")
+    //  }
 
- }
+  }
 
- handleChange(field, e){         
-     let fields = this.state.fields;
-     fields[field] = e.target.value;        
-     this.setState({fields});
- }
+  handleChange(field, e) {
+    let fields = this.state.fields;
+    fields[field] = e.target.value;
+    this.setState({ fields });
+  }
 
   render() {
     return (
       <div>
-        <PrimarySearchAppBar/>
-        <div> 
-        <h4 style={{ margin: 20,marginTop:100 }}>כתובת מייל</h4>          
-               <form name="contactform" className="contactform" onSubmit= {this.contactSubmit.bind(this)}>
-                    <div className="col-md-6">
-                      <fieldset>
-                       
-                          <br/>
-                         <TextField id="outlined-basic" label="Email" variant="outlined" refs="email" type="text"  placeholder="Email" onChange={this.handleChange.bind(this, "email")} value={this.state.fields["email"]}/>
-                         <br/>
-                         <span style={{color: "red"}}>{this.state.errors["email"]}</span>
-                         <br/>
+        <PrimarySearchAppBar />
+        <div>
+          <h4 style={{ margin: 20, marginTop: 100 }}>כתובת מייל</h4>
+          <form name="contactform" className="contactform" onSubmit={this.contactSubmit.bind(this)}>
+            <div className="col-md-6">
+              <fieldset>
 
-                         <br/>
-                     </fieldset>
-                  </div>
-                  <Button variant="contained" onClick={this.logIn}   type='submit'
-                  style={{backgroundColor:"#FAE8BE", fontSize:20,borderRadius:20,fontFamily:"Segoe UI",marginTop:20}}>אימות</Button>
-              </form>
+                <br />
+                <TextField id="outlined-basic" label="Email" variant="outlined" refs="email" type="text" placeholder="Email" onChange={this.handleChange.bind(this, "email")} value={this.state.fields["email"]} />
+                <br />
+                <span style={{ color: "red" }}>{this.state.errors["email"]}</span>
+                <br />
+
+                <br />
+              </fieldset>
             </div>
+            <Button variant="contained" onClick={this.logIn} type='submit'
+              style={{ backgroundColor: "#FAE8BE", fontSize: 20, borderRadius: 20, fontFamily: "Segoe UI", marginTop: 20 }}>אימות</Button>
+          </form>
+        </div>
       </div>
     )
   }
