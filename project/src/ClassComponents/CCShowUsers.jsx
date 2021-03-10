c09c0acbe16e62a4279a41278c5049a80e25d1e1
Fadido <ido5566@gmail.com>
2021-03-10 21:35:15 +0200

    add favorites functions

--- project/src/ClassComponents/CCShowUsers.jsx
+++ project/src/ClassComponents/CCShowUsers.jsx
@@ -13,20 +13,22 @@ import { MDBContainer } from "mdbreact";
 import "../scrollbar.css";
 
 const ScrollBarPage = () => {
-  const scrollContainerStyle = { width: "800px", maxHeight: "400px" };}
+    const scrollContainerStyle = { width: "800px", maxHeight: "400px" };
+}
 
-const filterByList = ["המחלקה שלי", "המחזור שלי","הישוב שלי"]
+const filterByList = ["המחלקה שלי", "המחזור שלי", "הישוב שלי"]
 const studArr = [];
 class CCShowUsers extends Component {
     constructor(props) {
         super(props);
         this.state = {
             studentstArr: studArr,
+            userMail: "",
             userDep: "",
             userYear: "",
-            userHomeTown:"",
-            text:""
-
+            userHomeTown: "",
+            text: "",
+            userFriendslist: []
 
 
         }
@@ -35,7 +37,12 @@ class CCShowUsers extends Component {
     componentDidMount() {
         let studOBJ = localStorage.getItem('student');
         studOBJ = JSON.parse(studOBJ);
-        this.setState({ userDep: studOBJ.DepName,userYear: studOBJ.StudyingYear ,userHomeTown:studOBJ.HomeTown})
+        this.setState({
+            userDep: studOBJ.DepName, userYear: studOBJ.StudyingYear,
+            userHomeTown: studOBJ.HomeTown,
+            userFriendslist: studOBJ.Friendslist !== null ? studOBJ.Friendslist : [],
+            userMail: studOBJ.Mail
+        })
 
 
         console.log(studOBJ.Mail)
@@ -80,10 +87,12 @@ class CCShowUsers extends Component {
                             default:
                                 break;
                         }
-                        let stud = { Mail: s.Mail, Fname: s.Fname, Lname: s.Lname, DateOfBirth: age,
-                             DepName: s.Dep.DepartmentName, HomeTown: s.HomeTown, StudyingYear: studYear,
-                              Photo: s.Photo, AddressStudying: s.AddressStudying,PersonalStatus:s.PersonalStatus,
-                              Plist:s.Plist,Hlist:s.Hlist }
+                        let stud = {
+                            Mail: s.Mail, Fname: s.Fname, Lname: s.Lname, DateOfBirth: age,
+                            DepName: s.Dep.DepartmentName, HomeTown: s.HomeTown, StudyingYear: studYear,
+                            Photo: s.Photo, AddressStudying: s.AddressStudying, PersonalStatus: s.PersonalStatus,
+                            Plist: s.Plist, Hlist: s.Hlist
+                        }
                         studArr.push(stud);
                     });
                     console.log(studArr);
@@ -98,7 +107,7 @@ class CCShowUsers extends Component {
         this.setState({ studentstArr: studArr });
         console.log(studArr)
     }
-   
+
     FilterUsers = (filterBy) => {
         if (filterBy == "המחלקה שלי") {
             let filterbydep = this.state.studentstArr.filter(s => s.DepName == this.state.userDep)
@@ -106,18 +115,18 @@ class CCShowUsers extends Component {
             if (filterbydep.length !== 0)
                 this.setState({ studentstArr: filterbydep })
             else
-                this.setState({ studentstArr: [],text:"אין תוצאות בסינון זה" })
+                this.setState({ studentstArr: [], text: "אין תוצאות בסינון זה" })
         }
-        if(filterBy=="choose"){
+        if (filterBy == "choose") {
             this.setState({ studentstArr: studArr });
         }
-        if(filterBy=="המחזור שלי"){
+        if (filterBy == "המחזור שלי") {
             let filterbyclass = this.state.studentstArr.filter(s => s.DepName == this.state.userDep && s.StudyingYear == this.state.userYear)
             console.log(filterbyclass);
             if (filterbyclass.length !== 0)
                 this.setState({ studentstArr: filterbyclass })
             else
-                this.setState({ studentstArr: [],text:"אין תוצאות בסינון זה"})
+                this.setState({ studentstArr: [], text: "אין תוצאות בסינון זה" })
         }
         if (filterBy == "הישוב שלי") {
             let filterbydep = this.state.studentstArr.filter(s => s.HomeTown == this.state.userHomeTown)
@@ -125,23 +134,36 @@ class CCShowUsers extends Component {
             if (filterbydep.length !== 0)
                 this.setState({ studentstArr: filterbydep })
             else
-                this.setState({ studentstArr: [] ,text:"אין תוצאות בסינון זה"})
+                this.setState({ studentstArr: [], text: "אין תוצאות בסינון זה" })
         }
 
     }
-    getData=(userPicked)=>{
+    getData = (userPicked) => {
         console.log(userPicked);
-        localStorage.setItem('chooseUser',JSON.stringify(userPicked));
+        localStorage.setItem('chooseUser', JSON.stringify(userPicked));
         this.props.history.push("/userProfile2");
-
     }
-    SearchUser=(val)=>{
-        let filtered_list = this.state.studentstArr.filter((item) =>item.Fname.includes(val)||item.Lname.includes(val));
-      if(val=="")
-        this.setState({ studentstArr: studArr });
-    else
-        this.setState({ studentstArr: filtered_list })
-    
+
+    getFavoriteData = (favMail) =>{
+        let studentToAdd ={
+            Mail: favMail
+        }
+        let newUserFriendslist=this.state.userFriendslist
+        newUserFriendslist.push(studentToAdd)
+        this.setState({userFriendslist: newUserFriendslist})
+        let studOBJ = localStorage.getItem('student');
+        studOBJ = JSON.parse(studOBJ);
+        studOBJ.Friendslist=newUserFriendslist;
+        localStorage.setItem('student', JSON.stringify(studOBJ));
+    }
+
+    SearchUser = (val) => {
+        let filtered_list = this.state.studentstArr.filter((item) => item.Fname.includes(val) || item.Lname.includes(val));
+        if (val == "")
+            this.setState({ studentstArr: studArr });
+        else
+            this.setState({ studentstArr: filtered_list })
+
     }
     render() {
         return (
@@ -150,51 +172,52 @@ class CCShowUsers extends Component {
 
 
                 <PrimarySearchAppBar />
-<div>
-                <h3 style={{ margin: 5, fontWeight: 'bold', direction: 'rtl', color: '#3D3D3D', fontSize: 26 }}>בוא נמצא חברים חדשים!</h3>
+                <div>
+                    <h3 style={{ margin: 5, fontWeight: 'bold', direction: 'rtl', color: '#3D3D3D', fontSize: 26 }}>בוא נמצא חברים חדשים!</h3>
 
-                <p style={{ color: '#3D3D3D', fontSize: 17 }}>גלה את החברים שלומדים איתך, שגרים ליידך </p>
-                <div style={{ marginBottom: 15 }}>
-                    <SearchField
-                    onChange={this.SearchUser}
-                        placeholder="Search..."
-                        
-                        // onChange={onChange}
-                        //searchText="חפש חבר"
-                        classNames="test-class"
-                    /></div>
+                    <p style={{ color: '#3D3D3D', fontSize: 17 }}>גלה את החברים שלומדים איתך, שגרים ליידך </p>
+                    <div style={{ marginBottom: 15 }}>
+                        <SearchField
+                            onChange={this.SearchUser}
+                            placeholder="Search..."
 
-                {/* <p className='labels'> סנן לפי </p> */}
-                <Select style={{ width: 200 , marginBottom: 10}} placeholder="סנן לפי"
-                    onChange={this.FilterUsers}>
-                    <Select.Option value="choose">כל המשתמשים</Select.Option>
-                    {filterByList.map((filterBy) => (
-                        <Select.Option key={filterBy} value={filterBy}>{filterBy}</Select.Option>
-                    ))}
-                </Select>
+                            // onChange={onChange}
+                            //searchText="חפש חבר"
+                            classNames="test-class"
+                        /></div>
+
+                    {/* <p className='labels'> סנן לפי </p> */}
+                    <Select style={{ width: 200, marginBottom: 10 }} placeholder="סנן לפי"
+                        onChange={this.FilterUsers}>
+                        <Select.Option value="choose">כל המשתמשים</Select.Option>
+                        {filterByList.map((filterBy) => (
+                            <Select.Option key={filterBy} value={filterBy}>{filterBy}</Select.Option>
+                        ))}
+                    </Select>
                 </div>
 
                 <h3 >{this.state.text}</h3>
-               
-      <div className="scrollbar my-5 mx-auto" style={{width: "100vw", maxHeight: "400px"}} >
-      
-      <div className='userList'>
-                    <Grid container >
-                        <Grid item xs={12}>
-                            <PerfectScrollbar>
-                                <Grid container justify="center" spacing={1}>
-                                    {this.state.studentstArr.map((s, index) => (
-                                        <Grid key={index} item>
-                                            <FCUserCard key={index} id={s.Mail} obj={s} name={s.Fname + " " + s.Lname} photo={s.Photo} studage={s.DateOfBirth} depName={s.DepName} year={s.StudyingYear} sendData={this.getData} />
-                                        </Grid>
-                                    ))}
-                                </Grid>
-                            </PerfectScrollbar>
+
+                <div className="scrollbar my-5 mx-auto" style={{ width: "100vw", maxHeight: "400px" }} >
+
+                    <div className='userList'>
+                        <Grid container >
+                            <Grid item xs={12}>
+                                <PerfectScrollbar>
+                                    <Grid container justify="center" spacing={1}>
+                                        {this.state.studentstArr.map((s, index) => (
+                                            <Grid key={index} item>
+                                                <FCUserCard key={index} id={s.Mail} obj={s} name={s.Fname + " " + s.Lname} photo={s.Photo} studage={s.DateOfBirth} depName={s.DepName} year={s.StudyingYear}
+                                                    isFavorite={this.state.userFriendslist.some((s1) => s1.Mail === s.Mail)} userMail={this.state.userMail} sendData={this.getData} sendFavoriteData={this.getFavoriteData}/>
+                                            </Grid>
+                                        ))}
+                                    </Grid>
+                                </PerfectScrollbar>
+                            </Grid>
                         </Grid>
-                    </Grid>
+                    </div>
                 </div>
-      </div>
-    
+
             </div>
         )
     }
--- project/src/FunctionalComponents/FCUserCard.jsx
+++ project/src/FunctionalComponents/FCUserCard.jsx
@@ -12,66 +12,104 @@ import SkipNextIcon from '@material-ui/icons/SkipNext';
 
 
 import { Progress } from 'antd';
+import { OmitProps } from 'antd/lib/transfer/ListBody';
 const useStyles = makeStyles((theme) => ({
-    root: {
-      display: 'flex',
-      width:350,
-      height:110
-    },
-    details: {
-      display: 'flex',
-      flexDirection: 'column',
-      width:160,
-      textAlign:'right',
-      padding:1
-    },
-    content: {
-      flex: '1 0 auto',
-      padding:1,
-      marginTop:25,
-      marginRight:5
-    },
-    cover: {
-      width: 80,
-      marginRight:0
-    },
-    controls: {
-      display: 'flex',
-      alignItems: 'center',
-      paddingLeft: theme.spacing(1),
-      paddingBottom: theme.spacing(1),
-    },
-    playIcon: {
-      height: 20,
-      width: 20,
-    },
-  }));
+  root: {
+    display: 'flex',
+    width: 350,
+    height: 110
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: 160,
+    textAlign: 'right',
+    padding: 1
+  },
+  content: {
+    flex: '1 0 auto',
+    padding: 1,
+    marginTop: 25,
+    marginRight: 5
+  },
+  cover: {
+    width: 80,
+    marginRight: 0
+  },
+  controls: {
+    display: 'flex',
+    alignItems: 'center',
+    paddingLeft: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
+  },
+  playIcon: {
+    height: 20,
+    width: 20,
+  },
+}));
 
-  export default function FCUserCard(props) {
-    const classes = useStyles();
-    const theme = useTheme();
-    const percentage = 66;
-    const chooseUser = () => {
-      props.sendData(props.obj);
+export default function FCUserCard(props) {
+  const classes = useStyles();
+  const theme = useTheme();
+  const percentage = 66;
+  const chooseUser = () => {
+    props.sendData(props.obj);
+  }
+
+  const AddToFavorites = () => {
+    console.log("in post favorite function");
+    let sf = {
+      Student1mail: props.userMail,
+      Student2mail: props.id
     }
+    let apiUrl = 'https://localhost:44325/api/students/AddToFavorites';
+    fetch(apiUrl,
+      {
+        method: 'POST',
+        body: JSON.stringify(sf),
+        headers: new Headers({
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Accept': 'application/json; charset=UTF-8'
+        })
+      })
+      .then(res => {
+        console.log('res=', res);
+        console.log('res.status', res.status);
+        if (res.status === 201) {
+          console.log('favorite created:)');
+        }
+        console.log('res.ok', res.ok);
 
-    return (
-      <Card className={classes.root} style={{ direction: 'rtl',width: "95vw"}} onClick={chooseUser}>
-                  <CardMedia
-          className={classes.cover}
-          image="/images/images (3).jfif"
-          title="Live from space album cover"
-        />
-        <div className={classes.details} style={{width:"50vw"}}>
-          <CardContent className={classes.content}>
-            <Typography component="h5" variant="h5" style={{fontFamily: "Segoe UI",fontSize:"6vw"}}>
-              {props.name+"   "+props.studage}
-            </Typography>
-            <Typography variant="subtitle1" color="textSecondary"  style={{fontFamily: "Segoe UI",fontSize:"3.8vw"}}>
-              {props.depName+" - "+props.year+"'"}
-            </Typography>
-          </CardContent>
-          {/* <div className={classes.controls}>
+        if (res.ok) {
+          console.log('post succeeded');
+          props.sendFavoriteData(props.id);
+        }
+      },
+        (error) => {
+          console.log("err post=", error);
+        });
+        console.log('end')
+}
+
+
+return (
+  <Card className={classes.root} style={{ direction: 'rtl', width: "95vw" }}>
+    <CardMedia
+      onClick={chooseUser}
+      className={classes.cover}
+      image="/images/images (3).jfif"
+      title="Live from space album cover"
+    />
+    <div className={classes.details} style={{ width: "50vw" }} onClick={chooseUser}>
+      <CardContent className={classes.content}>
+        <Typography component="h5" variant="h5" style={{ fontFamily: "Segoe UI", fontSize: "6vw" }}>
+          {props.name + "   " + props.studage}
+        </Typography>
+        <Typography variant="subtitle1" color="textSecondary" style={{ fontFamily: "Segoe UI", fontSize: "3.8vw" }}>
+          {props.depName + " - " + props.year + "'"}
+        </Typography>
+      </CardContent>
+      {/* <div className={classes.controls}>
             <IconButton aria-label="previous">
               {theme.direction === 'rtl' ? <SkipNextIcon /> : <SkipPreviousIcon />}
             </IconButton>
@@ -83,16 +121,18 @@ const useStyles = makeStyles((theme) => ({
               {theme.direction === 'rtl' ? <SkipPreviousIcon /> : <SkipNextIcon />}
             </IconButton>
           </div> */}
-        </div>
-        
-        <div style={{marginTop:30,width:"20vw"}}>
-<Progress type="circle" percent={percentage} strokeWidth={6} width={40}/>
-</div>
+    </div>
 
-        <div style={{width:40,marginTop:5}}>
-        <i className="bi-chat" style={{color:'#3D3D3D',fontSize:28}}></i>
-        <i className="bi-star" style={{color:'#3D3D3D',fontSize:28}}></i>
-        </div>
-      </Card>
-    );
-  }
+    <div style={{ marginTop: 30, width: "20vw" }}>
+      <Progress type="circle" percent={percentage} strokeWidth={6} width={40} />
+    </div>
+    
+    <div style={{ width: 40, marginTop: 5 }}>
+      <i className="bi-chat" style={{ color: '#3D3D3D', fontSize: 28 }}></i>
+      {props.isFavorite?
+      (<i className="bi-star-fill" style={{ color: '#3D3D3D', fontSize: 28 }} onClick={()=>alert("need delete")}></i>) :
+      (<i className="bi-star" style={{ color: '#3D3D3D', fontSize: 28 }} onClick={AddToFavorites}></i>) }
+    </div>
+  </Card>
+);
+}
