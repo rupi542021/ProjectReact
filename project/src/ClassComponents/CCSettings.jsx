8c5b258f49e48c4f738a1205e92493ae22fc185d
doronbatsir <rupi54.2021@gmail.com>
2021-06-20 12:39:44 +0300

    timeStemp

--- project/src/ClassComponents/CCSettings.jsx
+++ project/src/ClassComponents/CCSettings.jsx
@@ -30,8 +30,8 @@ class CCSettings extends Component {
 
  deleteProfile=(mail)=>{
   console.log("in deleteProfile ",mail );
-   let apiUrl = 'https://proj.ruppin.ac.il/igroup54/test2/A/tar5/api/students/'+ mail +'/deleteUserProfile';
-   //let apiUrl = 'https://localhost:44366/API/students/'+ mail +'/deleteUserProfile';
+  // let apiUrl = 'https://proj.ruppin.ac.il/igroup54/test2/A/tar5/api/students/'+ mail +'/deleteUserProfile';
+   let apiUrl = 'https://localhost:44325/API/students/'+ mail +'/deleteUserProfile';
     fetch(apiUrl,
       {
         method: 'Delete',
